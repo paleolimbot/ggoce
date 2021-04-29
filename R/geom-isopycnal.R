69b89678e7dd6ac9dd6c7968d07da9f080ee06bc
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2021-04-29 15:50:51 -0300

    add stub ggplot2 geom for isopycnals

--- R/geom-isopycnal.R
+++ R/geom-isopycnal.R
@@ -1,4 +1,61 @@
 
+#' Draw automagic isopycnals
+#'
+#' @inheritParams ggplot2::geom_point
+#'
+#' @return A [ggplot2::layer()]
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#' data(ctd, package = "oce")
+#'
+#' ggplot(ctd, aes(salinity, temperature)) +
+#'   geom_isopycnal() +
+#'   geom_point()
+#'
+geom_isopycnal <- function(mapping = NULL, data = NULL, stat = "identity",
+                           position = "identity", ..., na.rm = FALSE, show.legend = NA,
+                           inherit.aes = TRUE) {
+  ggplot2::layer(
+    data = data,
+    mapping = mapping,
+    stat = stat,
+    geom = GeomIsopycnal,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
+
+
+#' @rdname geom_isopycnal
+#' @export
+GeomIsopycnal <- ggplot2::ggproto(
+  "GeomIsopycnal",
+  ggplot2::Geom,
+
+  extra_params = "",
+
+  handle_na = function(data, params) {
+    data
+  },
+
+  default_aes = ggplot2::aes(x = NA_real_, y = NA_real_),
+
+  draw_panel = function(data, panel_params, coord) {
+    iso <- isopycnal_isolines(panel_params$x.range, panel_params$y.range)
+    iso <- lapply(iso, coord$transform, panel_params)
+
+    isoband::isolines_grob(
+      iso,
+      breaks = as.numeric(names(iso)),
+      units = "native"
+    )
+  }
+)
+
 #' Generate an 'isolines' object representing isopycnals
 #'
 #' @inheritParams oce::swRho
@@ -11,7 +68,7 @@
 #'   from objects.
 #' @param n_breaks Passed to `breaks` if `breaks` is a function.
 #'
-#' @return An object returned by [isoband::isolines()]. THe `x` values
+#' @return An object returned by [isoband::isolines()]. The `x` values
 #'   of the output refer to `salinity` and the `y` values of the output
 #'   refer to temperature.
 #' @export
