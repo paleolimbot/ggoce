a7c2584c57c67a6a3d0eda55b66a48d796fc2db0
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2021-04-30 16:20:16 -0300

    even more options for isopycnals

--- R/geom-isopycnal.R
+++ R/geom-isopycnal.R
@@ -21,11 +21,15 @@
 #'   geom_isopycnal() +
 #'   geom_point()
 #'
-geom_isopycnal <- function(mapping = NULL, data = NULL, stat = "identity",
-                           position = "identity", ...,
+geom_isopycnal <- function(mapping = NULL, data = NULL,
+                           ...,
                            lineend = "butt", linejoin = "round", linemitre = 10,
                            colour = "darkgray", size = 0.4, linetype = 1, alpha = NA,
                            text.size = 3, family = "",
+                           salinity_type = c("practical", "absolute"),
+                           temperature_type = c("in-situ", "conservative"),
+                           ref_pressure = 0,
+                           ref_longitude = NULL, ref_latitude = NULL,
                            trim_freezing = TRUE,
                            breaks = pretty,
                            n_breaks = 5,
@@ -33,12 +37,15 @@ geom_isopycnal <- function(mapping = NULL, data = NULL, stat = "identity",
                            eos = getOption("oceEOS", default = "gsw"),
                            na.rm = FALSE, show.legend = NA,
                            inherit.aes = TRUE) {
+  salinity_type <- match.arg(salinity_type)
+  temperature_type <- match.arg(temperature_type)
+
   ggplot2::layer(
     data = data,
     mapping = mapping,
-    stat = stat,
+    stat = "identity",
     geom = GeomIsopycnal,
-    position = position,
+    position = "identity",
     show.legend = show.legend,
     inherit.aes = inherit.aes,
     params = list(
@@ -47,6 +54,11 @@ geom_isopycnal <- function(mapping = NULL, data = NULL, stat = "identity",
       lineend = lineend, linejoin = linejoin, linemitre = linemitre,
       colour = colour, size = size, linetype = linetype, alpha = alpha,
       text.size = text.size, family = family,
+      salinity_type = salinity_type,
+      temperature_type = temperature_type,
+      ref_pressure = ref_pressure,
+      ref_longitude = ref_longitude,
+      ref_latitude = ref_latitude,
       trim_freezing = trim_freezing,
       breaks = breaks,
       n_breaks = n_breaks,
@@ -75,11 +87,18 @@ GeomIsopycnal <- ggplot2::ggproto(
   draw_panel = function(data, panel_params, coord,
                         lineend, linejoin, linemitre, colour, size, linetype, alpha,
                         text.size, family,
+                        salinity_type, temperature_type,
+                        ref_pressure, ref_latitude, ref_longitude,
                         trim_freezing,
                         breaks, n_breaks, n_sal, n_temp, eos) {
     # note: really need to backtransform range here
     iso <- isopycnal_isolines(
       panel_params$x.range, panel_params$y.range,
+      ref_pressure = ref_pressure,
+      ref_longitude = ref_longitude,
+      ref_latitude = ref_latitude,
+      salinity_type = salinity_type,
+      temperature_type = temperature_type,
       trim_freezing = trim_freezing,
       breaks = breaks,
       n_breaks = n_breaks,
@@ -112,7 +131,16 @@ GeomIsopycnal <- ggplot2::ggproto(
 
 #' Generate an 'isolines' object representing isopycnals
 #'
-#' @inheritParams oce::swRho
+#' @param salinity,salinity_type A salinity value. Use
+#'   `salinity_type = "absolute"` to specify that `salinity` values
+#'   are absolute salinities.
+#' @param temperature,temperature_type A temperature value. Use
+#'   `temperature_type = "conservative"`
+#'   to specify that these values are not in-situ temperature.
+#' @param ref_pressure The pressure that should be used to calculate
+#'   density contours in dbar.
+#' @param ref_longitude,ref_latitude The latitude and longitude that should
+#'   be used in seawater calculations if `eos` is "gsw".
 #' @param trim_freezing Don't draw contours outside the freezing region.
 #' @param n_sal Number of points with which contours should be approximated
 #'   on the salinity axis.
@@ -121,6 +149,8 @@ GeomIsopycnal <- ggplot2::ggproto(
 #' @param breaks A numeric vector or function used to generate breaks
 #'   from objects.
 #' @param n_breaks Passed to `breaks` if `breaks` is a function.
+#' @param eos The equation of state: one of "unesco" or "gsw". See
+#'   [oce::swRho()] for details.
 #'
 #' @return An object returned by [isoband::isolines()]. The `x` values
 #'   of the output refer to `salinity` and the `y` values of the output
@@ -137,34 +167,85 @@ GeomIsopycnal <- ggplot2::ggproto(
 #' }
 #'
 isopycnal_isolines <- function(salinity, temperature,
+                               ref_pressure = 0,
+                               ref_longitude = NULL,
+                               ref_latitude = NULL,
+                               salinity_type = c("practical", "absolute"),
+                               temperature_type = c("in-situ", "conservative"),
                                trim_freezing = TRUE,
                                breaks = pretty,
                                n_breaks = 5,
                                n_sal = 200, n_temp = 200,
                                eos = getOption("oceEOS", default = "gsw")) {
+
+  salinity_type <- match.arg(salinity_type)
+  temperature_type <- match.arg(temperature_type)
+
+  # usually this will be a range already, but can be useful to
+  # pass in an arbitrary vector of values as well.
   sal_range <- range(salinity, finite = TRUE)
   temp_range <- range(temperature, finite = TRUE)
 
-  # not configurable in oce::plotTS() for contours and maybe
-  # referencePressure is the wrong term
-  referencePressure <- 0
-
+  # It's important that these values are on a regular grid
+  # since they'll be used for coordinates of the contour lines
+  # and need to match the types of input `salinity` and `temperature`.
   sal_value <- seq(sal_range[1], sal_range[2], length.out = n_sal)
   temp_value <- seq(temp_range[1], temp_range[2], length.out = n_temp)
 
-  # create grid containing density values
+  # Make sure we have the right salinity and temperature types to calculate
+  # the in-situ density. To accommodate a mix of types, make sure we have
+  # practical salinity and in-situ temperature calculated.
   sal_grid <- vctrs::vec_rep_each(sal_value, n_temp)
   temp_grid <- vctrs::vec_rep(temp_value, n_sal)
-  if (identical(eos, "gsw")) {
-    rho_grid <- gsw::gsw_rho(sal_grid, temp_grid, referencePressure) - 1000
+
+  if (salinity_type == "absolute") {
+    assert_has_lonlat(ref_longitude, ref_latitude)
+
+    sal_grid_absolute <- sal_grid
+    sal_grid_practical <- gsw::gsw_SP_from_SA(
+      SA = sal_grid,
+      p = ref_pressure,
+      longitude = ref_longitude,
+      latitude = ref_latitude
+    )
+  } else if(temperature_type == "conservative") {
+    assert_has_lonlat(ref_longitude, ref_latitude)
+
+    sal_grid_practical <- sal_grid
+    sal_grid_absolute <- gsw::gsw_SA_from_SP(
+      SP = sal_grid,
+      p = ref_pressure,
+      longitude = ref_longitude,
+      latitude = ref_latitude
+    )
   } else {
-    rho_grid <- oce::swSigma(sal_grid, temp_grid, referencePressure, eos = "unesco")
+    sal_grid_practical <- sal_grid
+    sal_grid_absolute <- NULL
   }
 
+  if (temperature_type == "conservative") {
+    temp_grid_in_situ <- gsw::gsw_t_from_CT(sal_grid_absolute, temp_grid, ref_pressure)
+  } else {
+    temp_grid_in_situ <- temp_grid
+  }
+
+  # using in-situ density
+  rho_grid <- oce::swRho(
+    salinity = sal_grid_practical,
+    temperature = temp_grid_in_situ,
+    pressure = ref_pressure,
+    longitude = ref_longitude,
+    latitude = ref_latitude
+  ) - 1000
+
   if (trim_freezing) {
-    # technically using unesco even if eos is "gsw"
-    # to avoid location requirement (also in oce::plotTS())
-    is_freezing <- temp_grid < oce::swTFreeze(sal_grid, referencePressure, eos = "unesco")
+    is_freezing <- temp_grid_in_situ < oce::swTFreeze(
+      salinity = sal_grid_practical,
+      pressure = ref_pressure,
+      longitude = ref_longitude,
+      latitude = ref_latitude,
+      eos = eos
+    )
     rho_grid[is_freezing] <- NA_real_
   }
 
@@ -181,5 +262,12 @@ isopycnal_isolines <- function(salinity, temperature,
   isoband::isolines(
     sal_value, temp_value, rho_grid,
     levels = levels
+  )
+}
+
+assert_has_lonlat <- function(ref_longitude, ref_latitude) {
+  stopifnot(
+    !is.null(ref_longitude), length(ref_longitude) == 1, is.numeric(ref_longitude),
+    !is.null(ref_latitude), length(ref_latitude) == 1, is.numeric(ref_latitude),
   )
 }
--- tests/testthat/test-geom-isopycnal.R
+++ tests/testthat/test-geom-isopycnal.R
@@ -1,4 +1,14 @@
 
+test_that("ggplot() with geom_isopycnal() builds", {
+  # crude, but makes sure the code to render the plot runs at least once
+  tbl <- tibble::tibble(salinity = c(30, 31), temperature = c(4, 14))
+  p <- ggplot2::ggplot(tbl, ggplot2::aes(salinity, temperature)) +
+    geom_isopycnal() +
+    ggplot2::geom_point()
+  built <- expect_s3_class(ggplot2::ggplot_build(p), "ggplot_built")
+  expect_s3_class(ggplot2::ggplot_gtable(built), "gtable")
+})
+
 test_that("isopycnal_isolines() works", {
   iso <- isopycnal_isolines(30:31, 4:14)
   expect_s3_class(iso, "isolines")
@@ -12,7 +22,14 @@ test_that("isopycnal_isolines() works", {
 
 test_that("isopycnal_isolines() can use gsw", {
   iso <- isopycnal_isolines(30:31, 4:14)
-  iso_gsw <- isopycnal_isolines(30:31, 4:14, eos = "gsw")
+  iso_gsw <- isopycnal_isolines(30:31, 4:14, ref_latitude = 45, ref_longitude = -64, eos = "gsw")
   expect_identical(names(iso)[1:5], names(iso_gsw)[1:5])
 })
 
+test_that("isopycnal_isolines() works with conservative temperature", {
+  # stub to remind me to write this test
+})
+
+test_that("isopycnal_isoline() works with absolute salinity", {
+  # stub to remind me to write this test
+})
