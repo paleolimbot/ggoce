ec12914dd86b8a0e8e1ebf11dac02147fbcf2094
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2021-05-04 11:21:00 -0300

    add `which` arguments to fortify for ctd and section

--- R/fortify.R
+++ R/fortify.R
@@ -13,13 +13,34 @@
 #' data(ctd, package = "oce")
 #' fortify(ctd)
 #'
-fortify.ctd <- function(model, ...) {
-  tibble::tibble(!!! model@data, ...)
+fortify.ctd <- function(model, ..., which = c("data", "combined", "metadata")) {
+  which <- match.arg(which)
+
+  meta_lengths <- vapply(model@metadata, length, integer(1))
+  meta_vars <- names(model@metadata)[meta_lengths == 1]
+
+  data_lengths <- vapply(model@data, length, integer(1))
+  data_vars <- names(model@data)[data_lengths == length(model@data[[1]])]
+
+  tbl <- if (which == "data") {
+    tibble::tibble(!!! model@data[data_vars])
+  } else if (which == "metadata") {
+    tibble::tibble(!!! model@metadata[meta_vars])
+  } else if (which == "combined") {
+    data <- tibble::tibble(!!! model@data[data_vars])
+    meta <- tibble::tibble(!!! model@metadata[meta_vars])
+    vctrs::vec_cbind(meta, data)
+  } else {
+    stop(sprintf("Unsupported value for `which`: '%s'", which), call. = FALSE) # nocov
+  }
+
+  tbl_apply_dots(tbl, ...)
 }
 
 #' @rdname fortify.ctd
 #' @export
-fortify.section <- function(model, ...) {
+fortify.section <- function(model, ..., which = c("combined", "metadata", "data")) {
+  which <- match.arg(which)
 
   # not all metadata fields relate to the station and some are not
   # subset consistently (but stationId always is, so base the fields
@@ -31,17 +52,40 @@ fortify.section <- function(model, ...) {
     c("header", "filename", "sectionId")
   )
 
-  station_meta <- tibble::tibble(!!! model@metadata[station_meta_names], ...)
-  station_meta$distance <- oce::geodDist(model)
-  data_tbl <- lapply(model@data$station, fortify)
-  data_tbl_n <- vapply(data_tbl, nrow, integer(1))
+  tbl <- if (which == "data") {
+    data_tbl <- lapply(model@data$station, fortify, which = "data")
+    vctrs::vec_rbind(!!! data_tbl)
+  } else if (which == "metadata") {
+    station_meta <- tibble::tibble(!!! model@metadata[station_meta_names])
+    station_meta$distance <- oce::geodDist(model)
+    station_meta
+  } else if (which == "combined") {
+    station_meta <- tibble::tibble(!!! model@metadata[station_meta_names])
+    station_meta$distance <- oce::geodDist(model)
 
-  data_tbls <- vctrs::vec_rbind(!!! data_tbl)
+    data_tbl <- lapply(model@data$station, fortify, which = "data")
+    data_tbl_n <- vapply(data_tbl, nrow, integer(1))
+    data_tbls <- vctrs::vec_rbind(!!! data_tbl)
 
-  vctrs::vec_cbind(
-    vctrs::vec_rep_each(station_meta, data_tbl_n),
-    # in the off chance meta fields are also present in the ctd objects,
-    # the meta field wins
-    data_tbls[setdiff(names(data_tbls), names(station_meta))]
-  )
+    vctrs::vec_cbind(
+      vctrs::vec_rep_each(station_meta, data_tbl_n),
+      # in the off chance meta fields are also present in the ctd objects,
+      # the meta field wins
+      data_tbls[setdiff(names(data_tbls), names(station_meta))]
+    )
+  } else {
+    stop(sprintf("Unsupported value for `which`: '%s'", which), call. = FALSE) # nocov
+  }
+
+  tbl_apply_dots(tbl, ...)
+}
+
+# basically mutate()
+tbl_apply_dots <- function(tbl, ...) {
+  dots <- lapply(rlang::enquos(...), rlang::eval_tidy, data = tbl)
+  if (length(dots) > 0) {
+    vctrs::vec_cbind(tbl, tibble::as_tibble(dots))
+  } else {
+    tbl
+  }
 }
--- tests/testthat/test-fortify.R
+++ tests/testthat/test-fortify.R
@@ -7,7 +7,7 @@ test_that("fortify() works for ctd objects", {
   )
 
   expect_identical(
-    fortify(a),
+    fortify(a, which = "data"),
     tibble::tibble(
       scan = 1:3,
       salinity = 35 + 1:3 / 10,
@@ -15,15 +15,81 @@ test_that("fortify() works for ctd objects", {
       pressure = 1:3
     )
   )
+
+  expect_identical(
+    fortify(a, which = "metadata"),
+    tibble::tibble(
+      pressureType = "sea",
+      deploymentType = "unknown",
+      waterDepth = NA,
+      filename = "",
+      type = "",
+      serialNumber = ""
+    )
+  )
+
+  # without user-supplied dots
+  expect_identical(
+    fortify(a, which = "combined"),
+    vctrs::vec_cbind(
+      tibble::tibble(
+        pressureType = "sea",
+        deploymentType = "unknown",
+        waterDepth = NA,
+        filename = "",
+        type = "",
+        serialNumber = ""
+      ),
+      tibble::tibble(
+        scan = 1:3,
+        salinity = 35 + 1:3 / 10,
+        temperature = 10 - 1:3 / 10,
+        pressure = 1:3
+      )
+    )
+  )
+
+  # with user-supplied dots
+  expect_identical(
+    fortify(a, salinity2 = salinity * 2, which = "combined"),
+    vctrs::vec_cbind(
+      tibble::tibble(
+        pressureType = "sea",
+        deploymentType = "unknown",
+        waterDepth = NA,
+        filename = "",
+        type = "",
+        serialNumber = ""
+      ),
+      tibble::tibble(
+        scan = 1:3,
+        salinity = 35 + 1:3 / 10,
+        temperature = 10 - 1:3 / 10,
+        pressure = 1:3
+      ),
+      tibble::tibble(
+        salinity2 = (35 + 1:3 / 10) * 2
+      )
+    )
+  )
 })
 
 test_that("fortify() works for section objects", {
   data(section, package = "oce", envir = environment())
-  tbl <- fortify(section)
 
-  # check that metadata was added in the right order
   expect_identical(
-    tbl$stationId[1:6],
+    fortify(section, which = "metadata")$stationId,
+    section[["stationId"]]
+  )
+
+  expect_identical(
+    fortify(section, which = "data")$salinity,
+    unlist(lapply(section@data$station, "[[", "salinity"))
+  )
+
+  # check that metadata was added in the right order for which = "combined"
+  expect_identical(
+    fortify(section, which = "combined")$stationId[1:6],
     c("3", "3", "3", "3", "3", "4")
   )
 })
